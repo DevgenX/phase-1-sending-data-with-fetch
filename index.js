b3e0dfa7ec9262b66c9591b1d77785459abd3ea7
devgenx <sebastian110420@gmail.com>
2022-12-17 22:13:07 -0500

    Draft

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your code here
+const submitData = (name, email) => {
+  const DOM = document.querySelector("body");
+
+  return fetch("http://localhost:3000/users", {})
+    .then((response) => response.json())
+    .then((data) => {
+      DOM.append(data.id);
+    })
+    .catch((message) => {
+      DOM.append(message);
+    });
+};
